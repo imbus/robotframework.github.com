51a3fda23e724d42e978fe560b2c5c813e689d5b
olgaimb <olga.kyselova@imbus.de>
2024-07-24 18:06:32 +0200

    index.js changes

--- src/content/index.js
+++ src/content/index.js
@@ -14,6 +14,13 @@ import learning from './resources/german/learning.js'
 import users from './resources/german/users.js'
 import sponsors from './resources/german/sponsors.js'
 
+import builtin from './resources/builtin.js'
+import libraries from './resources/libraries.mjs'
+import tools from './resources/tools.mjs'
+import learning from './resources/learning.js'
+import users from './resources/users.js'
+import sponsors from './resources/sponsors.js'
+
 export {
   english,
   german,
