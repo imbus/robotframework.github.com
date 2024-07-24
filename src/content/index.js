d8aeaaeec18f213b77405302c8ca2c653a7da791
olgaimb <olga.kyselova@imbus.de>
2024-07-24 17:45:57 +0200

    import resources in index.js added

--- src/content/index.js
+++ src/content/index.js
@@ -7,6 +7,13 @@ import spanish from './spanish.js'
 import chinese from './chinese.js'
 import ach from './ach.js'
 
+import builtin from './resources/builtin.js'
+import libraries from './resources/libraries.mjs'
+import tools from './resources/tools.mjs'
+import learning from './resources/learning.js'
+import users from './resources/users.js'
+import sponsors from './resources/sponsors.js'
+
 export {
   english,
   german,
