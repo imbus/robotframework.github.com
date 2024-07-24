24e9229061a1ce1662997ecfd93162b7c989b378
Olga K <olga.kyselova@imbus.de>
2024-07-24 09:28:55 +0000

    Resources in English copied

--- src/content/index.js
+++ src/content/index.js
@@ -7,13 +7,6 @@ import spanish from './spanish.js'
 import chinese from './chinese.js'
 import ach from './ach.js'
 
-import builtin from './resources/builtin.js'
-import libraries from './resources/libraries.mjs'
-import tools from './resources/tools.mjs'
-import learning from './resources/learning.js'
-import users from './resources/users.js'
-import sponsors from './resources/sponsors.js'
-
 export {
   english,
   german,
@@ -22,12 +15,5 @@ export {
   russian,
   spanish,
   chinese,
-  ach,
-
-  builtin,
-  libraries,
-  tools,
-  learning,
-  users,
-  sponsors
+  ach
 }
